c566d13bd4a2cffb31b3b12c195f5463fc65105a
juelsonbessa <juelsonjunior27@gmail.com>
2024-09-27 23:56:09 +0100

    Mecanismo de pesquisa

--- Front-end/vitrine-cinema/src/App.jsx
+++ Front-end/vitrine-cinema/src/App.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 
 function App() {
   const [valueCategory, setValueCategory] = useState(28);
   const [movies, setMovies] = useState([]);
+  const inputSearch = useRef();
+  const [searchData, setSearchData] = useState([]);
   const API_KEY = "4d1e786585330f2ecc7c17fe00fc824b";
 
   const listCategory = async () => {
@@ -33,6 +35,22 @@ function App() {
     }
   };
 
+  const searchMovie = async (searchMovie) => {
+    const dataSerac = searchMovie.current.value;
+
+    try {
+      const dataSearch = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${dataSerac}&language=pt-BR`
+      );
+
+      const filteredSerach = dataSearch.data.results.slice(0, 5);
+
+      setSearchData(filteredSerach);
+    } catch (error) {
+      console.log("Erro: ", error);
+    }
+  };
+
   useEffect(() => {
     listCategory();
     listMovies();
@@ -48,8 +66,12 @@ function App() {
             type="search"
             className="h-10 shadow-md rounded-full p-4"
             placeholder="Pesquise por filmes"
+            ref={inputSearch}
           />
-          <button className="bg-blue-500 w-24 rounded-full text-white text-sm active:scale-95">
+          <button
+            className="bg-blue-500 w-24 rounded-full text-white text-sm active:scale-95"
+            onClick={() => searchMovie(inputSearch)}
+          >
             Pesquisar
           </button>
         </div>
@@ -68,36 +90,65 @@ function App() {
           </div>
 
           <div className="flex flex-wrap items-center justify-center gap-5 mt-4">
-            {movies.length > 0 &&
-              movies.map((movie, index) => (
-                <div
-                  className="rounded-lg overflow-hidden shadow-lg relative"
-                  key={index}
-                >
-                  <div className="absolute top-5 left-1/2 transform -translate-x-1/2 bg-blue-500 rounded-full w-10 h-10 flex items-center justify-center animate-pulse">
-                    <span className="text-white">
-                      {Math.floor(movie.vote_average)}
-                    </span>
-                  </div>
+            {searchData.length > 0
+              ? searchData.map((search, index) => (
+                  <div
+                    className="rounded-lg overflow-hidden shadow-lg relative"
+                    key={index}
+                  >
+                    <div className="absolute top-5 left-1/2 transform -translate-x-1/2 bg-blue-500 rounded-full w-10 h-10 flex items-center justify-center animate-pulse">
+                      <span className="text-white">
+                        {Math.floor(search.vote_average)}
+                      </span>
+                    </div>
 
-                  <div className="w-52 h-72">
-                    <img
-                      src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                      alt=""
-                      className="w-52 h-full object-cover"
-                    />
-                  </div>
-                  <div className="flex flex-col p-1">
-                    <p className="font-bold break-words overflow-wrap break-word w-48">
-                      {movie.title}
-                    </p>
+                    <div className="w-52 h-72">
+                      <img
+                        src={`https://image.tmdb.org/t/p/w200${search.poster_path}`}
+                        alt=""
+                        className="w-52 h-full object-cover"
+                      />
+                    </div>
+                    <div className="flex flex-col p-1">
+                      <p className="font-bold break-words overflow-wrap break-word w-48">
+                        {search.title}
+                      </p>
 
-                    <span className="text-slate-400 font-bolg">
-                      {movie.release_date}
-                    </span>
+                      <span className="text-slate-400 font-bolg">
+                        {search.release_date}
+                      </span>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))
+              : movies.map((movie, index) => (
+                  <div
+                    className="rounded-lg overflow-hidden shadow-lg relative"
+                    key={index}
+                  >
+                    <div className="absolute top-5 left-1/2 transform -translate-x-1/2 bg-blue-500 rounded-full w-10 h-10 flex items-center justify-center animate-pulse">
+                      <span className="text-white">
+                        {Math.floor(movie.vote_average)}
+                      </span>
+                    </div>
+
+                    <div className="w-52 h-72">
+                      <img
+                        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                        alt=""
+                        className="w-52 h-full object-cover"
+                      />
+                    </div>
+                    <div className="flex flex-col p-1">
+                      <p className="font-bold break-words overflow-wrap break-word w-48">
+                        {movie.title}
+                      </p>
+
+                      <span className="text-slate-400 font-bolg">
+                        {movie.release_date}
+                      </span>
+                    </div>
+                  </div>
+                ))}
           </div>
         </div>
       </div>
