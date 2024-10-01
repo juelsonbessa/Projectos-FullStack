416ba3e2f34b99eaf01b9e7b268784d79ad6487a
juelsonbessa <juelsonjunior27@gmail.com>
2024-10-01 17:12:25 +0100

    Instalando o cors
    Para permitir o acesso do front ao back

--- Back-end/login-vsf/index.js
+++ Back-end/login-vsf/index.js
@@ -2,9 +2,14 @@ import express from "express";
 import publicRoute from "./routers/public.js"
 import privateRoute from "./routers/private.js"
 import auth from "./middleware/auth.js"
+import cors from "cors"
+
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
 app.use('/', publicRoute)
 app.use('/', auth, privateRoute)
 
